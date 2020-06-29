7ef9ffd7bf47f50ab1fc8e753b0af9ddb4a891ff
Lucas Sanches <lcbsanches@gmail.com>
2020-06-29 14:44:42 -0300

    Refatoração e Comentários

--- app.js
+++ app.js
@@ -7,65 +7,63 @@ function getNumberType() {
     if (type[i].checked) {
       txt = type[i].value; //A variavél txt fica com o valor do tipo que foi selecionado
       switch(txt) {        //Fazemos um switch para iniciar a conversão
-        case "decimal":    //Caso decimal, convertemos a string em número de base decimal que ficará em num
-          num = parseInt(numberInput.value, 10);
-
-          if(!num){ //Caso o usuário passe um valor inválido, um alerto vai avisá-lo e o código é interrompido
+        case "decimal":    //Caso decimal
+          num = parseInt(numberInput.value, 10);//Convertemos o valor para decimal
+          /**
+           * Mesmo já sendo decimal, o número que passamos vem como uma string
+           * então usamos o parseInt para se tornar uma variável numérica e
+           * assim podemos converter a base numérica com toString
+           */
+          if(!num){ //Caso o valor não possa ser convertido, um alerta vai avisá-lo
             alert("Please, input a valid number.")
             break;  
           }
 
-          document.getElementById("decimalOutput").innerHTML = num.toString(10); //Retorna uma string em decimal
-          document.getElementById("binaryOutput").innerHTML = num.toString(2);//Retorna uma string em binário
-          document.getElementById("octalOutput").innerHTML = num.toString(8);//Retorna uma string em octal
-          document.getElementById("hexadecimalOutput").innerHTML = num.toString(16).toUpperCase();//Retorna uma string em hexa
+          numToString(num);
           break;
 
-        case "binary":  //Caso binário, o número binário é convertido em decimal, e repetimos o processos anteriores
-          num = parseInt(numberInput.value, 2);
+        case "binary"://Caso binário
+          num = parseInt(numberInput.value, 2);//Convertemos em decimal
 
           if(!num){
             alert("Please, input a valid number.")
             break;
           }
 
-          document.getElementById("decimalOutput").innerHTML = num.toString(10);
-          document.getElementById("binaryOutput").innerHTML = num.toString(2);
-          document.getElementById("octalOutput").innerHTML = num.toString(8);
-          document.getElementById("hexadecimalOutput").innerHTML = num.toString(16).toUpperCase();
+          numToString(num);
           break;
 
-        case "octal":
-          num = parseInt(numberInput.value, 8);
+        case "octal"://Caso octal
+          num = parseInt(numberInput.value, 8);//Convertemos em decimal
 
           if(!num){
             alert("Please, input a valid number.")
             break;
           }
 
-          document.getElementById("decimalOutput").innerHTML = num.toString(10);
-          document.getElementById("binaryOutput").innerHTML = num.toString(2);
-          document.getElementById("octalOutput").innerHTML = num.toString(8);
-          document.getElementById("hexadecimalOutput").innerHTML = num.toString(16).toUpperCase();
+          numToString(num);
           break;
 
-        case "hexadecimal":
-          num = parseInt(numberInput.value, 16);
+        case "hexadecimal"://Caso hexadecimal
+          num = parseInt(numberInput.value, 16);//Convertemos em decimal
 
           if(!num){
             alert("Please, input a valid number.")
             break;
           }
 
-          document.getElementById("decimalOutput").innerHTML = num.toString(10);
-          document.getElementById("binaryOutput").innerHTML = num.toString(2);
-          document.getElementById("octalOutput").innerHTML = num.toString(8);
-          document.getElementById("hexadecimalOutput").innerHTML = num.toString(16).toUpperCase();
+          numToString(num);
           break;
 
         default:
       }
     }
-  }
-  console.log(txt); 
+  }  
+}
+
+function numToString(num) {
+  document.getElementById("decimalOutput").innerHTML = num.toString(10); //Retorna uma string em decimal
+  document.getElementById("binaryOutput").innerHTML = num.toString(2);//Retorna uma string em binário
+  document.getElementById("octalOutput").innerHTML = num.toString(8);//Retorna uma string em octal
+  document.getElementById("hexadecimalOutput").innerHTML = num.toString(16).toUpperCase();//Retorna uma string em hexa
 }
